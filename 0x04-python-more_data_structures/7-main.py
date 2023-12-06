ef0bee5b5ee7b5d359d5d5281408568bf1968b95
Chadrack Ndalamba <chadrackndalamba@gmail.com>
2023-12-06 04:22:19 -0800

    tasks

--- 0x04-python-more_data_structures/7-main.py
+++ 0x04-python-more_data_structures/7-main.py
@@ -2,7 +2,7 @@
 update_dictionary = __import__('7-update_dictionary').update_dictionary
 print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
 new_dict = update_dictionary(a_dictionary, 'language', "Python")
 print_sorted_dictionary(new_dict)
 print("--")
@@ -11,7 +11,10 @@ print_sorted_dictionary(a_dictionary)
 print("--")
 print("--")
 
-new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
+# Break the long line into multiple lines to comply with the 79 characters limit
+new_dict = update_dictionary(
+    a_dictionary, 'city', "San Francisco"
+)
 print_sorted_dictionary(new_dict)
 print("--")
 print_sorted_dictionary(a_dictionary)
