dbdc1c0749d4035beb12bfc759c96cffbcc31902
Chad96 <chadrackndalamba@gmail.com>
2024-02-13 13:06:34 +0200

    my files

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Displays all records of the table second_table.
+-- Records are ordered in descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,6 @@
+-- Displays all records of the table second_table having a name value in my MySQL server.
+-- Records are ordered by a descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
