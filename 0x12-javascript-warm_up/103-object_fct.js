13d9ce2198c7b66574ef4d16362416f013387f48
Chad96 <chadrackndalamba@gmail.com>
2024-03-11 18:46:13 +0200

    task

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-
 const myObject = {
   type: "object",
   value: 12,
 };
 console.log(myObject);
-myObject.incr = function () {
-  this.value++;
+myObject.incr = () => {
+  myObject.value += 1;
 };
 myObject.incr();
 console.log(myObject);
