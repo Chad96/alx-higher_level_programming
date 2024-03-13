b660d8e2b114b2addb3143854387ed66e8f44646
Chad96 <chadrackndalamba@gmail.com>
2024-03-12 20:59:25 -0700

    tasks

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv.length <= 3) {
-  console.log("0");
+  console.log('0');
 } else {
   const arr = process.argv.slice(2).map(Number);
   const second = arr.sort(function (a, b) {
