3418d046df5d635a7a0f29cacf07046c6dd07d21
Chad96 <chadrackndalamba@gmail.com>
2024-03-27 22:01:37 +0200

    task

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#takes in a URL as an argument, sends a GET request to the URL
-curl -sH "X_School-User-Id: 98" "$1"
+# header var X-HolbertonSchool-User-Id sent with value 98
+curl -s "$1" -H "X-School-User-Id: 98"
