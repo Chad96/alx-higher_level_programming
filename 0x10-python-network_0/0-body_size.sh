07bcc14c6e388eb425181be37c3d927e6a15b4fa
Chad96 <chadrackndalamba@gmail.com>
2024-03-27 21:29:44 +0200

    tasks

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,18 @@
-#!/bin/bash
-# this script takesi URL and sends request to display the size in bytes
-curl -sI "$1" | grep -i "Content_Length" | awk '{print$2}'
+#!/usr/bin/env bash
+# Check if a URL is provided as an argument
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <URL>"
+    exit 1
+fi
+
+url="$1"
+
+# Use curl to send a request and retrieve the size of the response body
+response_size=$(curl -sI "$url" | grep -i Content-Length | awk '{print $2}' | tr -d '\r\n')
+
+# Check if Content-Length header is present in the response
+if [ -z "$response_size" ]; then
+    echo "Content-Length header not found in the response."
+    exit 1
+fi
