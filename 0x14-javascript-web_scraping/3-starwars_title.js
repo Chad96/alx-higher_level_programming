7bec57151e524a429aae3c68766704050495360b
Chad96 <chadrackndalamba@gmail.com>
2024-04-23 18:56:31 +0200

    tasks

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 // Import the 'request' module.
-const request = require("request");
+const request = require('request');
 
 // Construct the URL for the specific Star Wars film
-const url = "https://swapi-api.alx-tools.com/api/films/" + process.argv[2];
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
 // Use the 'request' module to perform an HTTP GET request to the constructed URL.
 request(url, function (error, response, body) {
