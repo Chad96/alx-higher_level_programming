d11aa4157ba60a942c5993e0e0997b2d7c3445b3
Chad96 <chadrackndalamba@gmail.com>
2024-04-23 18:52:36 +0200

    tasks

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 // Import the 'request' module.
 
 request
   .get(process.argv[2])
   // Use the 'request' module to perform an HTTP GET request to the URL.
 
-  .on("response", function (response) {
+  .on('response', function (response) {
     // Set up an event listener for the 'response' event emitted by the HTTP request.
 
     console.log(`code: ${response.statusCode}`);
