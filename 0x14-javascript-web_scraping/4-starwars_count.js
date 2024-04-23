79a413b50cfc5e00600f1f515a5094b1bf701d54
Chad96 <chadrackndalamba@gmail.com>
2024-04-23 19:06:47 +0200

    tasks

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 // Import the 'request' module
-const request = require('request');
+const request = require("request");
 
 // Use the 'request' module to perform an HTTP GET request to the URL
 request(process.argv[2], function (error, response, body) {
@@ -13,7 +13,7 @@ request(process.argv[2], function (error, response, body) {
     console.log(
       results.reduce((count, movie) => {
         // check if there is a character with ID 18 ('/18/') in the 'characters' array.
-        return movie.characters.find((character) => character.endsWith('/18/'))
+        return movie.characters.find((character) => character.endsWith("/18/"))
           ? // If a character with ID 18 is found, increment the count by 1.
             count + 1
           : // Otherwise, keep the count unchanged.
